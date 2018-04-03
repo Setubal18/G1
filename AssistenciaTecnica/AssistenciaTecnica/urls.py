de6c81ca58cbd72881bb111ce48cfff8eab3aacd
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-02 21:49:40 -0300

    models melhorados

--- AssistenciaTecnica/AssistenciaTecnica/urls.py
+++ AssistenciaTecnica/AssistenciaTecnica/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# import suport.views
 from django.contrib import admin
 from django.urls import path
 
+from suport.views import chamada
+
 urlpatterns = [
+    path('chamada/', chamada, name='chamada'),
     path('admin/', admin.site.urls),
+
 ]
--- AssistenciaTecnica/suport/views.py
+++ AssistenciaTecnica/suport/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from .models import *
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
+
+def chamada(request):
+    chamada = Chamada.objects.all()
+    chamadas = {'lista': chamada}
+    return render(request,'home.html', chamadas)
+
+def chamada_list(request, pk):
+    chamada= get_object_or_404(Chamada,pk=pk)
+    return render(request,'post.html', {'chamada':chamada})
